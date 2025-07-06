1221bc3d54006d133167734748bf584742ab299e
joaobiava <138793503+joaobiava@users.noreply.github.com>
2025-07-06 14:33:53 -0300

    acho que o trabalho tá feito

---
+++ trabalho-final/arquivosJS/switch_black_white.js
@@ -0,0 +1,5 @@
+const toggleBtn = document.getElementById("toggle-theme");
+toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+    toggleBtn.textContent = document.body.classList.contains("dark-mode") ? "☀️" : "🌙";
+});
